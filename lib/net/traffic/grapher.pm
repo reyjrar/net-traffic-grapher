62aa545424c47322f8c6117b2911ac88e2415b57
Brad Lhotsky <brad.lhotsky@gmail.com>
2010-09-02 12:29:58 -0400

    Removed Debug component and set default view to Mason

--- lib/net/traffic/grapher.pm
+++ lib/net/traffic/grapher.pm
@@ -13,7 +13,6 @@ use Catalyst::Runtime 5.80;
 #                 directory
 
 use Catalyst qw/
-    -Debug
     ConfigLoader
     Static::Simple
 /;
@@ -36,6 +35,7 @@ __PACKAGE__->config(
     name => 'net::traffic::grapher',
     # Disable deprecated behavior needed by old applications
     disable_component_resolution_regex_fallback => 1,
+	default_view => 'Mason',
 );
 
 # Start the application
