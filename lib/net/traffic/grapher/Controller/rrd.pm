838b56e44b7ea08866ded975ed62602296175afb
Brad Lhotsky <brad.lhotsky@gmail.com>
2010-09-02 12:30:16 -0400

    RRD::Service added

--- lib/net/traffic/grapher/Controller/rrd.pm
+++ lib/net/traffic/grapher/Controller/rrd.pm
@@ -4,6 +4,7 @@ use namespace::autoclean;
 use File::Spec;
 use File::Basename;
 use Path::Class;
+use File::Find::Rule;
 
 BEGIN {extends 'Catalyst::Controller'; }
 
@@ -18,6 +19,13 @@ Catalyst Controller.
 =head1 METHODS
 
 =cut
+
+my %_spans = (
+	halfday => { start => 3600*12, step => 60 },
+	day 	=> { start => 3600*24, step => 60 },
+	week 	=> { start => 3600*24*7, step => 60 },
+	month 	=> { start => 3600*24*30, step => 60 },
+);
 
 
 =head2 index
@@ -34,28 +42,28 @@ sub rrd_base :Chained('/') :PathPart('rrd') :CaptureArgs(0) {
 	my ($self,$c) = @_;
 }
 
-sub syslog_data :Chained('rrd_base') :PathPart('syslog') :CaptureArgs(1) {
-	my ( $self, $c, $srv ) = @_;
-	my $dir = dir( File::Spec->catdir($c->stash->{datadir}, 'syslog') );
+sub service_rrd :Chained('rrd_base') :PathPart('service') :CaptureArgs(1) {
+	my ($self,$c,$service) = @_;
 
-	my %file = ();
+	# Untaint
+	$service =~ s/[^a-z0-9]+//g;
 
-	my $abs = $dir->file($srv . '.rrd')->absolute->stringify;
-	if( -f $abs ) {
-		%file = (  name => $srv, file => $abs );
-	}
-	else {
-		die "rrd_graph(syslog): unknown srv '$srv'";
+	# Do we have files
+	my @rrds = File::Find::Rule->file()->name( lc($service) . '.rrd' )->in( $c->stash->{rrd_base} );
+	if( !@rrds ) {
+		die "service_rrd($service): no data";
 	}
 
-	$c->stash->{type} = 'syslog';
-	$c->stash->{id} = $srv;
-	$c->stash->{rrd} = \%file;
+	$c->stash->{service} = $service;
+	$c->stash->{rrds} = \@rrds;
 }
 
-sub syslog_view :Chained('syslog_data') :PathPart('view') :Args(2) {
-	my ( $self, $c, $type, $span ) = @_;
+sub service_view :Chained('service_rrd') :PathPart('view') :Args(2) {
+	my ($self,$c,$type,$span) = @_;
 
+	my @rrds = @{ $c->stash->{rrds} };
+
+	# Type Definition
 	my %_defs = (
 		bytes => {
 			tcp_bytes	=> { title => 'TCP Bytes', color => '66FF66' },
@@ -66,43 +74,42 @@ sub syslog_view :Chained('syslog_data') :PathPart('view') :Args(2) {
 			udp_packets	=> { title => 'UDP Packets', color => 'FF6666' },
 		},
 	);
-	my $time = time;
-	my %_spans = (
-		halfday 	=> { start => $time - 3600*12, end => $time, step => 60 },
-		day 	=> { start => $time - 3600*24, end => $time, step => 60 },
-		week 	=> { start => $time - 3600*24*7, end => $time, step => 60 },
-		month 	=> { start => $time - 3600*24*30, end => $time, step => 60 },
-	);
 
-	# Defaults
-	if( !defined $type || !exists $_defs{$type} ) {
-		$type = 'bytes';
+	if( !exists $_defs{$type} ) {
+		die "service_view($type): non-existant type";
 	}
-	if( !defined $span || !exists $_spans{$span} ) {
-		$span = 'day';
+	if( !exists $_spans{$span} ) {
+		die "service_view($span): unknown time span";
 	}
 
-	
-	# Build Lines	
+	# Build out the Defintions & Artifacts
 	my @defs = ();
 	my @artifacts = ();
-	my $rrd = $c->stash->{rrd};
-	foreach my $var ( keys %{ $_defs{$type} } ) {
-		my $info_ref = $_defs{$type}->{$var};
-		push @defs, qq{DEF:$var=$rrd->{file}:$var:AVERAGE};
-		push @artifacts, qq{AREA:$var#$info_ref->{color}:$info_ref->{title}:STACK};
+	foreach my $rrd (@rrds) {
+		my @dirs = File::Spec->splitdir( dirname( $rrd ) );
+		my $proto = pop @dirs;
+		my $varname = join('_', $proto, $type);
+		next unless exists $_defs{$type}->{$varname};
+		my $info_ref = $_defs{$type}->{$varname};
+
+		push @defs, qq{DEF:$varname=$rrd:$type:AVERAGE};
+		push @artifacts, qq{AREA:$varname#$info_ref->{color}:$info_ref->{title}:STACK};
 	}
+	# Build the Time
+	my $time = time;
 
 	$c->stash->{graphopts} = {
-		%{ $_spans{$span} },
+		start => $time - $_spans{$span}->{start},
+		end	=> $time,
+		step => $_spans{$span}->{step},
 		defs => \@defs,
 		artifacts => \@artifacts,		
-		title => $c->stash->{type} . ' - ' . $c->stash->{id} . ", past $span",
+		title => $c->stash->{service} . ", past $span",
 		'vertical-label' => $type,
 	};
 
+	# Display the graph
 	$c->detach('/rrd/display_graph');
-
 }
 
 sub display_graph :Private {
@@ -138,9 +145,9 @@ sub auto :Private {
 
 
 	$c->stash->{current_view} = 'RRDGraph';
-	$c->stash->{datadir} = $c->config->{rrd}{datadir};
-	if( ! -d $c->stash->{datadir} ) {
-		die "rrd/datadir not defined in config!";
+	$c->stash->{rrd_base} = $c->config->{rrd}{dir};
+	if( ! -d $c->stash->{rrd_base} ) {
+		die "rrd/dir not defined in config!";
 	}
 	return 1;
 }
